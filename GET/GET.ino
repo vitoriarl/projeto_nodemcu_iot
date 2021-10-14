1b0dc7a64a9f06decfafc0b08ec3e56662402500
vitoriarl <70915784+vitoriarl@users.noreply.github.com>
2021-10-13 21:05:27 -0400

    Add files via upload

--- GET/GET.ino
+++ GET/GET.ino
@@ -1,24 +1,36 @@
-#include "ESP8266HTTPClient.h"
+// biblioteca para enviar as requisicoes http
+#include <ESP8266HTTPClient.h>
+
+// bibliotecas para conectar o esp8266 ao wi-fi
 #include <ESP8266WiFi.h>
-#include <DallasTemperature.h>
 #include <WiFiClient.h>
+
+// bibliotecas para ler a temperatura
+#include <DallasTemperature.h>
+#include <OneWire.h>
+
+// biblioteca para enviar notificação para o bot do telegram
 #include <CTBot.h>
+
 //Instancia os objetos
-#define barramento 2 //Pino de sinal (amarelo) do DS18b20 ligado ao pino 4
-String BOT_TOKEN = "2099943113:AAFIjD92tu3qia0v9wEQcQ2wt5xnEpMsy_w";
-const int CHAT_ID = 789976451;
+#define barramento 2 //Pino de sinal (amarelo) do DS18b20 ligado ao pino 2
 OneWire oneWire(barramento);
 DallasTemperature ds18b20(&oneWire);
-WiFiClient client; //cliente responsável pela conexão
+WiFiClient client; // cliente responsável pela conexao
 CTBot myBot;
-//Declaração das variáveis
+
+//Declaracao das variaveis
 const char* ssid = "MiMi"; 
 const char* senha = "113126virus";
-char servidor[] = "http://192.168.88.229/tcc2/enviaDados.php";//HTTPS não vai funcionar devido ao sertificado SSL
+char servidor[] = "http://192.168.88.229/tcc2/enviaDados.php";
+String BOT_TOKEN = "2099943113:AAFIjD92tu3qia0v9wEQcQ2wt5xnEpMsy_w"; // token do telegram
+const int CHAT_ID = 789976451; // id do telegram
 
 void setup() 
 {
   Serial.begin(115200);
+
+  // conexão do microcontrolador ao wi-fi
   WiFi.begin(ssid, senha);
   //permanece neste laço até a conexão
   while (WiFi.status() != WL_CONNECTED) 
@@ -26,34 +38,34 @@ void setup()
     delay(500); 
     Serial.print("."); 
   }
-  ds18b20.begin();
+  ds18b20.begin(); // inicializa o sensor
+
+  // inicializacao da biblioteca para o telegram
   Serial.println("Inicializando TelegramBot...");
 
-  // connect the ESP8266 to the desired access point
+  // insere usuario e senha do wi-fi
   myBot.wifiConnect(ssid, senha);
 
-  // set the telegram bot token
+  // insere o token do telegram
   myBot.setTelegramToken(BOT_TOKEN);
   
-  // check if all things are ok
+  // checa se a conexão está ok
   if (myBot.testConnection())
-    Serial.println("\ntestConnection OK");
+    Serial.println("\nConexao ON");
   else
-    Serial.println("\ntestConnection NOK");
+    Serial.println("\nConexao OFF");
 }
 
 void loop() 
 {
-  TBMessage msg;
-  Enviar();
-  
-  //espera por 15 minutos
+  Enviar(); 
+  //espera
   delay(90000);
-  
 }
 
 void Enviar()
 {
+    // media de temperatura
     double leitura = 0, media = 0;
     for (int i = 0; i < 10; i++)
     {
@@ -62,17 +74,20 @@ void Enviar()
         delay(100);
     }
     media = leitura / 10;
+    
+    // transforma a media de temperatura em string
     String tempe = String(media);
+    
     //cria o objeto http, que posibilita acessar páginas da web
     HTTPClient http;
+
+    // envia a requisicao http GET com a temperatura
     String url = "?temp=" + tempe;
     http.begin(client, servidor + url);
     http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-    int httpCode = http.POST(url);//está POST pois se preiso usar GET, basta formar a URL, mas pode usar PORT, includive com GET também
+    int httpCode = http.POST(url);
     if (httpCode == 200)
     {
-        //se cair aqui, deu tudo certo
-        //retorno é a variável que retorna informações do PHP, qualquer coisa que você programar
         String retorno = http.getString();
         if(retorno == "0")
         {
@@ -81,13 +96,14 @@ void Enviar()
     }
     else
     {
-        //se cair aqui, deu erro;
+        
     }
-    //fecha a conexão
+    //fecha a conexao
     http.end();
 }
 
+// envia mensagem para o bot do telegram
 void sendTelegramMessage() {
-  String message = "Temperatura Alta !";
+  String message = "Temperatura Alta!";
   myBot.sendMessage(CHAT_ID, message);
 }
